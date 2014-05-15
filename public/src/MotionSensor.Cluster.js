e9d28a7f45fe03492cdc50bb3d32acd89baed41a
Gerardo Bort <gerardobort@gmail.com>
2014-05-15 16:19:09 -0700

    cluster refactor

--- public/src/MotionSensor.Cluster.js
+++ public/src/MotionSensor.Cluster.js
@@ -14,17 +14,84 @@
         this.debugColor = debugColor;
     };
 
-    var swapProp = function (a, b, prop) {
-        var tmpVal = a[prop];
-        a[prop] = b[prop];
-        b[prop] = tmpVal;
-    };
-    MotionSensor.Cluster.prototype.swap = function (cluster) {
-        swapProp(this, cluster, 'centroid');
-        swapProp(this, cluster, 'points');
-        swapProp(this, cluster, 'pixels');
-        swapProp(this, cluster, 'boundaryPointIndices');
-    };
+    MotionSensor.Cluster.randomPointColors = [];
+    var r, g, b;
+    for (var i = 0; i < 100; i++) {
+        r = Math.floor(Math.random()*255);
+        g = Math.floor(Math.random()*255);
+        b = Math.floor(Math.random()*255);
+        MotionSensor.Cluster.randomPointColors.push('rgba(' + r + ', ' + g + ', ' + b + ', 0.6)');
+    }
 
+    MotionSensor.Cluster.upsertArrayFromPoints = function (previousClusters, points, totalClusters) {
+        var k = totalClusters,
+            p = O = P = null,
+            dMin = d = 0,
+            jMin = 0,
+            clusters = [],
+            step = 0,
+            maxSteps = 3,
+            maxDelta = 70;
+
+        // PAM algorythm (k-Means clustering)
+        for (step = 0; step < maxSteps; step++) {
+            for (j = 0; j < k; j++) {
+                if (0 === step) {
+                    if (!previousClusters[j]) {
+                        x = parseInt(Math.random()*VIDEO_WIDTH, 10);
+                        y = parseInt(Math.random()*VIDEO_HEIGHT, 10);
+                        clusters.push(new MotionSensor.Cluster(
+                            j,
+                            [x, y],
+                            [1, 0],
+                            0,
+                            [],
+                            [],
+                            [],
+                            [0,0],
+                            [0,0,0],
+                            MotionSensor.Cluster.randomPointColors[j]
+                        ));
+                    } else {
+                        clusters.push(previousClusters[j]);
+                    }
+                } else {
+                    // re-assign cluster x,y
+                    clusters[j].centroid.x = clusters[j].acum[0]/clusters[j].points.length;
+                    clusters[j].centroid.y = clusters[j].acum[1]/clusters[j].points.length;
+                    clusters[j].points = [];
+                    clusters[j].pixels = [];
+                    clusters[j].boundaryPointIndices = [];
+                    clusters[j].acum = [0, 0];
+                }
+            }
+
+            for (i = 0, l = points.length; i < l; i++) {
+                p = points[i];
+                dMin = Number.MAX_VALUE;
+                jMin = 0;
+                for (j = 0; j < k; j++) {
+                    O = [clusters[j].centroid.x, clusters[j].centroid.y];
+                    P = [p.position.x, p.position.y];
+                    if ((d = p.position.getDistance(clusters[j].centroid)) < dMin) {
+                        dMin = d;
+                        jMin = j;
+                    }
+                }
+                if ((step !== maxSteps-1 || dMin < maxDelta) && clusters[jMin]) {
+                    clusters[jMin].points.push(p.position);
+                    clusters[jMin].pixels.push(p);
+                    clusters[jMin].acum[0] += p.position.x;
+                    clusters[jMin].acum[1] += p.position.y;
+                } else if (step < maxSteps) {
+                    // means color
+                    clusters[jMin].rgbFloat[0] += p.color.x/l;
+                    clusters[jMin].rgbFloat[1] += p.color.y/l;
+                    clusters[jMin].rgbFloat[2] += p.color.z/l;
+                }
+            }
+        }
+        return clusters;
+    }
 
 }(window, document));
--- public/src/MotionSensor.Processor.js
+++ public/src/MotionSensor.Processor.js
@@ -14,14 +14,6 @@
         this.context = motionSensor.canvasContext;
 
         this.i = 0; // frame counter
-        this.randomPointColors = [];
-        var r, g, b;
-        for (var i = 0; i < 100; i++) {
-            r = Math.floor(Math.random()*255);
-            g = Math.floor(Math.random()*255);
-            b = Math.floor(Math.random()*255);
-            this.randomPointColors.push('rgba(' + r + ', ' + g + ', ' + b + ', 0.6)');
-        }
         this.clustersBuffer = [];
         this.convexHull = new this.ConvexHull();
     }
@@ -49,14 +41,8 @@
             h = VIDEO_HEIGHT = this.motionSensor.VIDEO_HEIGHT;
 
         var k = this.motionSensor.options.totalClusters,
-            p = O = P = null,
-            dMin = d = 0,
-            jMin = 0,
-            points = [],
             clusters = [],
-            step = 0,
-            maxSteps = 3,
-            maxDelta = 70;
+            points = [];
 
         // iterate through the main buffer and calculate the differences with previous
         for (i = 0; i < len; i += 4) {
@@ -92,66 +78,12 @@
             }
         }
 
+        // compute clusters
+        clusters = MotionSensor.Cluster.upsertArrayFromPoints(this.clustersBuffer, points, k);
+
+        // paint canvas with the latest camera frame
         ctx.putImageData(imageDataBuffers[imageDataBuffersN-1], 0, 0);
 
-        // PAM algorythm (k-Means clustering)
-        for (step = 0; step < maxSteps; step++) {
-            for (j = 0; j < k; j++) {
-                if (0 === step) {
-                    if (!this.clustersBuffer[j]) {
-                        x = parseInt(Math.random()*VIDEO_WIDTH, 10);
-                        y = parseInt(Math.random()*VIDEO_HEIGHT, 10);
-                        clusters.push(new MotionSensor.Cluster(
-                            j,
-                            [x, y],
-                            [1, 0],
-                            0,
-                            [],
-                            [],
-                            [],
-                            [0,0],
-                            [0,0,0],
-                            this.randomPointColors[j]
-                        ));
-                    } else {
-                        clusters.push(this.clustersBuffer[j]);
-                    }
-                } else {
-                    // re-assign cluster x,y
-                    clusters[j].centroid.x = clusters[j].acum[0]/clusters[j].points.length;
-                    clusters[j].centroid.y = clusters[j].acum[1]/clusters[j].points.length;
-                    clusters[j].points = [];
-                    clusters[j].pixels = [];
-                    clusters[j].boundaryPointIndices = [];
-                    clusters[j].acum = [0, 0];
-                }
-            }
-
-            for (i = 0, l = points.length; i < l; i++) {
-                p = points[i];
-                dMin = Number.MAX_VALUE;
-                jMin = 0;
-                for (j = 0; j < k; j++) {
-                    O = [clusters[j].centroid.x, clusters[j].centroid.y];
-                    P = [p.position.x, p.position.y];
-                    if ((d = p.position.getDistance(clusters[j].centroid)) < dMin) {
-                        dMin = d;
-                        jMin = j;
-                    }
-                }
-                if ((step !== maxSteps-1 || dMin < maxDelta) && clusters[jMin]) {
-                    clusters[jMin].points.push(p.position);
-                    clusters[jMin].pixels.push(p);
-                    clusters[jMin].acum[0] += p.position.x;
-                    clusters[jMin].acum[1] += p.position.y;
-                } else if (step < maxSteps) {
-                    // means color
-                    clusters[jMin].rgbFloat[0] += p.color.x/l;
-                    clusters[jMin].rgbFloat[1] += p.color.y/l;
-                    clusters[jMin].rgbFloat[2] += p.color.z/l;
-                }
-            }
-        }
 
         for (var j = 0; j < k; j++) {
             var cluster = clusters[j];
@@ -162,6 +94,8 @@
 
             // draw object hulls
             cluster.boundaryPointIndices = this.convexHull.getGrahamScanPointIndices(cluster.points);
+
+            // compute cluster motion values
             if (cluster.boundaryPointIndices && cluster.boundaryPointIndices.length > 0) {
 
                 /// <---
@@ -243,28 +177,6 @@
                 this.clustersBuffer[j] = cluster; // update buffer
             }
         }
-
-        // collide and swap clusters
-        /*
-        var tmpCluster, tmpCentroid;
-        for (var j = 0; j < k; j++) {
-            for (var z = 0; z < j; z++) {
-                if (this.i < 60) {
-                    continue;
-                }
-                if (tmpCluster = this.clustersBuffer[j]) {
-                    tmpCentroid = new MotionSensor.Vector2(
-                        tmpCluster.centroid.x + tmpCluster.versor.x * tmpCluster.modulus,
-                        tmpCluster.centroid.y + tmpCluster.versor.y * tmpCluster.modulus
-                    );
-                    if (this.clustersBuffer[z].centroid.getDistance(tmpCentroid) < 16) {
-                        console.log('swap');
-                        this.clustersBuffer[j].swap(this.clustersBuffer[z]);
-                    }
-                }
-            }
-        }
-        */
 
         this.motionSensor.trigger('processor:compute', [
             this.clustersBuffer,
