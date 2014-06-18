968e1462172d7ce33d64b9150faa0212b1773b28
Gerardo Bort <gerardobort@gmail.com>
2014-06-18 12:41:15 -0700

    file structure refactor for static servers compatibility

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ var express = require('express'),
     app = express();
 
 app
-    .use(express.static('./public'))
-    .use(express.static('./src'))
+    .use(express.static('.'))
     .listen(process.env.PORT || 3000, function () {
         console.log('Motion Sensor sample server is listening on port ' + (process.env.PORT || 3000));
     });
