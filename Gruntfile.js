33c7095c17efe09a1760a9302982b2d598d04e73
Gerardo Bort <gerardobort@gmail.com>
2014-06-18 10:56:52 -0700

    adding non-minified version to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,47 @@
 module.exports = function(grunt) {
 
     var properties = {
-        srcPath: 'public/src'
+        srcPath: 'public/src',
+        packageFiles: [
+            "<%= properties.srcPath %>/MotionSensor.js",
+            "<%= properties.srcPath %>/MotionSensor.PerformanceController.js",
+            "<%= properties.srcPath %>/MotionSensor.Cluster.js",
+            "<%= properties.srcPath %>/MotionSensor.Processor.js",
+            "<%= properties.srcPath %>/MotionSensor.ConvexHull.js",
+        ]
     };
 
     grunt.config.set('properties', properties);
 
     grunt.initConfig({
         properties: properties,
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: properties.packageFiles,
+                dest: 'dist/motion-sensor.js',
+            },
+        },
         uglify: {
             options: {
                 // preserveComments: 'all'
             },
-            target: {
+            dist: {
                 files: {
-                    'dist/motion-sensor.min.js': [
-                        "<%= properties.srcPath %>/MotionSensor.js",
-                        "<%= properties.srcPath %>/MotionSensor.PerformanceController.js",
-                        "<%= properties.srcPath %>/MotionSensor.Cluster.js",
-                        "<%= properties.srcPath %>/MotionSensor.Processor.js",
-                        "<%= properties.srcPath %>/MotionSensor.ConvexHull.js",
-                    ],
+                    'dist/motion-sensor.min.js': properties.packageFiles,
                 }
             }
         },
     });
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('compile', [
-        'uglify:target',
+        'concat:dist',
+        'uglify:dist',
     ]);
 
 };
