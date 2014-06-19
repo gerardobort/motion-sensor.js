4b95fb8bd27393b13ff612c82682f799006fd6de
Gerardo Bort <gerardobort@gmail.com>
2014-06-19 12:42:17 -0700

    4th dimentional distance tests

--- dist/motion-sensor.js
+++ dist/motion-sensor.js
@@ -151,7 +151,10 @@
     };
 
     MotionSensor.Vector4.prototype.getDistance = function(v4) {
-        return Math.sqrt(0.5*Math.pow(this.x - v4.x, 2) + 0.5*Math.pow(this.y - v4.y, 2) + 10*Math.pow(this.z - v4.z, 2) + 10*Math.pow(this.t - v4.t, 2));
+        // two pairs distance
+        var d1 = Math.sqrt(Math.pow(this.x - v4.x, 2) + Math.pow(this.y - v4.y, 2));
+        var d2 = Math.sqrt(Math.pow(this.z - v4.z, 2) + Math.pow(this.t - v4.t, 2));
+        return Math.sqrt(0.1*Math.pow(d1, 2) + Math.pow(d2, 2));
     };
 
     MotionSensor.Vector4.prototype.dotProduct = function(k) {
