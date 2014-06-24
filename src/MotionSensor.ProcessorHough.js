9a31a5616f2aecbd66738f367e66f988912d37d0
Gerardo Bort <gerardobort@gmail.com>
2014-06-24 03:08:26 -0300

    adding hough debug tools - inverse mapping almost done

--- src/MotionSensor.ProcessorHough.js
+++ src/MotionSensor.ProcessorHough.js
@@ -102,6 +102,30 @@
         }
     }
 
+    // idea based on: http://liquify.eu/project/HoughTransform/
+    // math calculus taken from: https://github.com/yume190/TenSha/blob/756acdf0d9562525350578eb7603f7fae63abe30/Q1/tool/OpenCV2-Python-master/Official_Tutorial_Python_Codes/3_imgproc/houghlines.py
+    MotionSensor.ProcessorHough.prototype.houghLine = function (rho, theta) {
+            rho = rho - this.rhoMax/2;
+            //theta = theta / 360 * Math.PI; 
+            //var a = -Math.cos(theta);
+            //var b = Math.sin(theta);
+            var thetaIndex = Math.floor(theta);
+            var a = this.cosTable[thetaIndex];
+            var b = this.sinTable[thetaIndex];
+            
+            var x0 = a*rho;
+            var y0 = b*rho;
+            var x1 = x0 + 1000*-b;
+            var y1 = y0 + 1000* a;
+            var x2 = x0 - 1000*-b;
+            var y2 = y0 - 1000* a;
+
+            return [
+                new MotionSensor.Vector2(x1, y1),
+                new MotionSensor.Vector2(x2, y2)
+            ]
+    }
+
     MotionSensor.ProcessorHough.prototype.processCircularBuffer = function (originalImageData, imageDataBuffers) {
         this.i++;
 
@@ -151,8 +175,8 @@
         }
 
         for (i = 0; i < houghpx.length; i += 4) {
-            x = (i/4) % w;
-            y = parseInt((i/4) / w);
+            x = (i/4) % this.numAngleCells;
+            y = parseInt((i/4) / this.numAngleCells);
 
             if ((!(x % SAMPLING_GRID_FACTOR) && !(y % SAMPLING_GRID_FACTOR)) && houghpx[i+3] > MOTION_ALPHA_THRESHOLD) {
                 points.push(
@@ -166,23 +190,37 @@
 
 
         if (this.motionSensor.options.debug) {
+            // video canvas mark
             ctx.beginPath();
             ctx.fillStyle = '#f00';
             ctx.fillRect(w - (this.mouseX - 2), this.mouseY - 2, 4, 4);
             ctx.closePath();
+            // hough canvas line
             this.houghAcc(this.mouseX, this.mouseY, 255, 0, 0, 100, houghpx);
         }
 
         houghctx.putImageData(houghBuffer, 0, 0);
 
         if (this.motionSensor.options.debug) {
+            // hough canvas mark
             houghctx.beginPath();
             houghctx.fillStyle = '#f00';
-            houghctx.fillRect(/*this.numAngleCells -*/(this.houghMouseX - 2), this.houghMouseY - 2, 4, 4);
+            houghctx.fillRect((this.houghMouseX - 2), this.houghMouseY - 2, 4, 4);
             houghctx.closePath();
+            // video canvas line
+
+            var linePoints = this.houghLine(this.houghMouseY, this.houghMouseX);
+
+            ctx.beginPath();
+            ctx.moveTo(linePoints[0].x, linePoints[0].y);
+            ctx.lineTo(linePoints[1].x, linePoints[1].y);
+            ctx.closePath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = '#f00';
+            ctx.stroke();
         }
 
-        clusters = MotionSensor.Cluster.upsertArrayFromPoints(this.clustersBuffer, points, k, this.motionSensor, 1, this.canvas.width, this.canvas.height);
+        clusters = MotionSensor.Cluster.upsertArrayFromPoints(this.clustersBuffer, points, k, this.motionSensor, 1, this.numAngleCells, this.rhoMax);
 
         for (var j = 0, k = clusters.length; j < k; j++) {
             var cluster = clusters[j];
@@ -199,14 +237,9 @@
                 cluster.centroid.y = (cluster.centroid.y + this.clustersBuffer[j].centroid.y)*.5;
             }
 
-            var rho = cluster.centroid.y;
-            var theta = Math.floor(cluster.centroid.x);
-
-            var ox = w/2 + (rho - (h/2)*this.sinTable[theta])/this.cosTable[theta];
-            var oy = h/2 + (rho - (w/2)*this.cosTable[theta])/this.sinTable[theta];
-
-            cluster.p1 = new MotionSensor.Vector2(ox-rho*this.cosTable[theta], oy-rho*this.sinTable[theta]);
-            cluster.p2 = new MotionSensor.Vector2(ox, oy);
+            var clusterLinePoints = this.houghLine(cluster.centroid.y, cluster.centroid.x); 
+            cluster.p1 = clusterLinePoints[0];
+            cluster.p2 = clusterLinePoints[1];
 
             this.clustersBuffer[j] = cluster; // update buffer
         }
