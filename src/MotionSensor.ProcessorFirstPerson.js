e5bf6ac77ac7b8ddb2185d6c5dbd4f70ef1b5fbe
Gerardo Bort <gerardobort@gmail.com>
2014-08-15 22:00:21 -0700

    some progress on firstperson

--- src/MotionSensor.ProcessorFirstPerson.js
+++ src/MotionSensor.ProcessorFirstPerson.js
@@ -39,11 +39,44 @@
             gamma = 3,
             i = l = x = y = 0,
             w = VIDEO_WIDTH = this.motionSensor.VIDEO_WIDTH,
-            h = VIDEO_HEIGHT = this.motionSensor.VIDEO_HEIGHT;
+            h = VIDEO_HEIGHT = this.motionSensor.VIDEO_HEIGHT,
+            vanishingLineY1 = h * 0.4;
+            vanishingLineY2 = h * 0.6;
 
         var k = this.motionSensor.options.totalClusters,
             clusters = [],
-            points = [];
+            point;
+
+
+
+        // compute clusters
+        //clusters = MotionSensor.Cluster.upsertArrayFromPoints(this.clustersBuffer, points, k, this.motionSensor);
+        this.clustersBuffer = clusters = [
+            new MotionSensor.Cluster({
+                id: 'above',
+                centroid: new MotionSensor.Vector2(w/2, vanishingLineY1/2),
+                versor: new MotionSensor.Vector2(0, -1),
+                modulus: 0,
+                points: [],
+                pixels: [],
+                boundaryPointIndices: [],
+                acum: [0, 0],
+                rgbFloat: new MotionSensor.Vector3(0, 255, 0),
+                debugColor: MotionSensor.Cluster.randomPointColors[j]
+            }),
+            new MotionSensor.Cluster({
+                id: 'below',
+                centroid: new MotionSensor.Vector2(w/2, vanishingLineY2 + (h - vanishingLineY2)/2),
+                versor: new MotionSensor.Vector2(0, 1),
+                modulus: 0,
+                points: [],
+                pixels: [],
+                boundaryPointIndices: [],
+                acum: [0, 0],
+                rgbFloat: new MotionSensor.Vector3(0, 255, 0),
+                debugColor: MotionSensor.Cluster.randomPointColors[j]
+            })
+        ];
 
         // iterate through the main buffer and calculate the differences with previous
         for (i = 0; i < len; i += 4) {
@@ -57,22 +90,24 @@
 
             x = (i/4) % w;
             y = parseInt((i/4) / w);
-            if (this.i > imageDataBuffersN && (!(x % SAMPLING_GRID_FACTOR) && !(y % SAMPLING_GRID_FACTOR)) && alpha > MOTION_ALPHA_THRESHOLD) {
+            if (this.i > imageDataBuffersN && (!(x % SAMPLING_GRID_FACTOR) && !(y % SAMPLING_GRID_FACTOR)) && alpha > MOTION_ALPHA_THRESHOLD && (y < vanishingLineY1 || y > vanishingLineY2)) {
+                if (Math.random() > 0.3) {
+                    continue;
+                }
                 if (this.motionSensor.options.debug) {
                     newpx[i+3] = parseInt(alpha, 10);
                 }
-                points.push(
-                    new MotionSensor.Pixel(
-                        new MotionSensor.Vector2(x, y),
-                        new MotionSensor.Vector3(newpx[i+0], newpx[i+1], newpx[i+2])
-                    )
+                var point = new MotionSensor.Pixel(
+                    new MotionSensor.Vector2(x, y),
+                    new MotionSensor.Vector3(newpx[i+0], newpx[i+1], newpx[i+2])
                 );
+                if (point.position.y < vanishingLineY1) {
+                    clusters[0].points.push(point);
+                } else if (point.position.y > vanishingLineY2) {
+                    clusters[1].points.push(point);
+                }
             }
         }
-
-
-        // compute clusters
-        clusters = MotionSensor.Cluster.upsertArrayFromPoints(this.clustersBuffer, points, k, this.motionSensor);
 
         // paint canvas with the latest camera frame
         ctx.putImageData(imageDataBuffers[imageDataBuffersN-1], 0, 0);
@@ -85,91 +120,105 @@
                 continue;
             }
 
-            // draw object hulls
-            cluster.boundaryPointIndices = this.convexHull.getGrahamScanPointIndices(cluster.points);
+            /// <---
+            var p, nx, ny, dx, dy, q, prevpx, c1, c2, cx = cy = countx = county = 0, maxpx = 30, pcounter = 0;
 
-            // compute cluster motion values
-            if (cluster.boundaryPointIndices && cluster.boundaryPointIndices.length > 0) {
+            for (i = 0, l = cluster.points.length; i < l; i++) {
+                x = cluster.points[i].position.x;
+                y = cluster.points[i].position.y;
 
-                /// <---
-                var p, nx, ny, dx, dy, q, prevpx, c1, c2, cx = cy = countx = county = 0, maxpx = 30, pcounter = 0;
+                prevpx = imageDataBuffers[imageDataBuffersN-2].data;
+                lastpx = imageDataBuffers[imageDataBuffersN-1].data;
 
-                for (i = 0, l = cluster.points.length; i < l; i++) {
-                    x = cluster.points[i].x;
-                    y = cluster.points[i].y;
+                q = (y*w + x)*4;
+                c1 = [lastpx[q+0], lastpx[q+1], lastpx[q+2]];
 
-                    prevpx = imageDataBuffers[imageDataBuffersN-2].data;
-                    lastpx = imageDataBuffers[imageDataBuffersN-1].data;
+                cx = cy = 0;
 
-                    q = (y*w + x)*4;
-                    c1 = [lastpx[q+0], lastpx[q+1], lastpx[q+2]];
-
-                    cx = cy = 0;
-
-                    for (dx = 0; dx < maxpx; dx++) {
-                        nx = x + dx;
-                        q = (y*w + nx)*4;
-                        c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
-                        if (distance3(c1, c2, 0) < 50) {
-                            cx++;
-                        } else {
-                            break;
-                        }
+                for (dx = 0; dx < maxpx; dx++) {
+                    nx = x + dx;
+                    q = (y*w + nx)*4;
+                    c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
+                    if (distance3(c1, c2, 0) < 50) {
+                        cx++;
+                    } else {
+                        break;
                     }
-                    for (dx = 0; dx > -maxpx; dx--) {
-                        nx = x + dx;
-                        q = (y*w + nx)*4;
-                        c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
-                        if (distance3(c1, c2, 0) < 50) {
-                            cx--;
-                        } else {
-                            break;
-                        }
-                    }
-                    for (dy = 0; dy < maxpx; dy++) {
-                        ny = y + dy;
-                        q = (ny*w + x)*4;
-                        c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
-                        if (distance3(c1, c2, 0) < 50) {
-                            cy++;
-                        } else {
-                            break;
-                        }
-                    }
-                    for (dy = 0; dy > -maxpx; dy--) {
-                        ny = y + dy;
-                        q = (ny*w + x)*4;
-                        c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
-                        if (distance3(c1, c2, 0) < 50) {
-                            cy--;
-                        } else {
-                            break;
-                        }
-                    }
-                    countx += cx;
-                    county += cy;
-                    pcounter++;
                 }
-
-                cluster.modulus = Math.sqrt(countx*countx + county*county);
-                if (cluster.modulus) {
-                    cluster.versor.x = -countx/cluster.modulus;
-                    cluster.versor.y = -county/cluster.modulus;
-                } else {
-                    cluster.versor.x = 1;
-                    cluster.versor.y = 0;
+                for (dx = 0; dx > -maxpx; dx--) {
+                    nx = x + dx;
+                    q = (y*w + nx)*4;
+                    c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
+                    if (distance3(c1, c2, 0) < 50) {
+                        cx--;
+                    } else {
+                        break;
+                    }
                 }
-                cluster.modulus *= (0.006 / Math.pow(this.motionSensor.scale,2));
-
-                if (this.clustersBuffer[j]) { // ease centroid movement by using buffering
-                    cluster.centroid.x = (cluster.centroid.x + this.clustersBuffer[j].centroid.x)*.5;
-                    cluster.centroid.y = (cluster.centroid.y + this.clustersBuffer[j].centroid.y)*.5;
+                for (dy = 0; dy < maxpx; dy++) {
+                    ny = y + dy;
+                    q = (ny*w + x)*4;
+                    c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
+                    if (distance3(c1, c2, 0) < 50) {
+                        cy++;
+                    } else {
+                        break;
+                    }
                 }
-
-                ///  --->
-                this.clustersBuffer[j] = cluster; // update buffer
+                for (dy = 0; dy > -maxpx; dy--) {
+                    ny = y + dy;
+                    q = (ny*w + x)*4;
+                    c2 = [prevpx[q+0], prevpx[q+1], prevpx[q+2]];
+                    if (distance3(c1, c2, 0) < 50) {
+                        cy--;
+                    } else {
+                        break;
+                    }
+                }
+                countx += cx;
+                county += cy;
+                pcounter++;
             }
+
+            cluster.modulus = Math.sqrt(countx*countx + county*county);
+            if (cluster.modulus) {
+                cluster.versor.x = -countx/cluster.modulus;
+                cluster.versor.y = -county/cluster.modulus;
+            } else {
+                cluster.versor.x = 1;
+                cluster.versor.y = 0;
+            }
+            cluster.modulus *= (0.006 / Math.pow(this.motionSensor.scale,2));
+
+            if (this.clustersBuffer[j]) { // ease centroid movement by using buffering
+                cluster.centroid.x = (cluster.centroid.x + this.clustersBuffer[j].centroid.x)*.5;
+                cluster.centroid.y = (cluster.centroid.y + this.clustersBuffer[j].centroid.y)*.5;
+            }
+
+            if (this.motionSensor.testVideo || this.motionSensor.testImage) {
+                cluster.versor.x *= -1;
+            }
+
+            ///  --->
+            this.clustersBuffer[j] = cluster; // update buffer
         }
+        //console.log(clusters[0].versor, clusters[1].versor);
+
+        this.context.beginPath();
+        this.context.moveTo(0, vanishingLineY1);
+        this.context.lineTo(w, vanishingLineY1);
+        this.context.closePath();
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = '#0f0';
+        this.context.stroke();
+
+        this.context.beginPath();
+        this.context.moveTo(0, vanishingLineY2);
+        this.context.lineTo(w, vanishingLineY2);
+        this.context.closePath();
+        this.context.lineWidth = 2;
+        this.context.strokeStyle = '#0f0';
+        this.context.stroke();
 
 
         this.motionSensor.trigger('processor:compute', [
