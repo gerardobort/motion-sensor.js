3692eff2db3971df46c5a64ff7767564da39377e
Gerardo Bort <gerardobort@gmail.com>
2016-04-02 18:18:15 -0300

    gh-pages doesnt need nodejs stuff

--- server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express'),
-    app = express();
-
-app
-    .use(express.static('.'))
-    .listen(process.env.PORT || 3000, function () {
-        console.log('Motion Sensor sample server is listening on port ' + (process.env.PORT || 3000));
-    });
